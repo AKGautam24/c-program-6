c050f7479c09464261344d368db88bd43b144c20
AKGautam24 <anujgautam03012018@gmail.com>
2024-10-04 15:58:22 +0530

    Add files via upload

---
+++ musk.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    int m;
+    printf("enter the value m:");
+    scanf("%d", &m);
+
+    if(m != 0)
+     if(m > 0) {
+        printf("the value of n =1");
+    } else {
+        printf("the value of n=-1");
+      } 
+      else {
+        printf("the value of n= 0");
+      }
+}
